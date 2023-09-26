b63c9dee8d7b3e71b9b5834be5385aa3140244b9
walter <143214414+wal561606@users.noreply.github.com>
2023-09-26 13:04:41 -0300

    Update Header.js se agregó enlace en imagen

--- react-pescaderiaCJ/src/components/layout/Header.js
+++ react-pescaderiaCJ/src/components/layout/Header.js
@@ -5,8 +5,9 @@ const Header = (props) =>  {
     return (
         <header>
             <div className="holder">
+                <a href="/">
                 <img className="logocap" src="images/images/logo1.png" width="120" alt="Pescadería Capitan Jack" />
-                
+                </a>
                 <h1>Capitan JACK Pescaderías</h1>
                 <h3>Rafaela-Santa Fe</h3>
                 
