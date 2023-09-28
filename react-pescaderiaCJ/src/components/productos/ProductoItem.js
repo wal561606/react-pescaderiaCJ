e928781e30503db302a039dc06148e4230998558
walter <walter561606@ufasta.edu.ar>
2023-09-28 20:41:31 -0300

    Se agrega modal a productos para contacto con el negocio

--- react-pescaderiaCJ/src/components/productos/ProductoItem.js
+++ react-pescaderiaCJ/src/components/productos/ProductoItem.js
@@ -1,23 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Modal from 'react-bootstrap/Modal';
+import { ImWhatsapp } from 'react-icons/im';
 
 import '../../styles/components/pages/ProductoItem.css';
 
 const ProductoItem = (props) => {
-    const { key, name, price, image, description } = props;
-    return (
-        <section className='seccionproductos'>
-                  
+  const { key, name, price, image, description } = props;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
+  return (
+    <section className='seccionproductos'>
       <Row xs={1} md={0} className="g-6">
-          <Col key={key}>
-          <Card className="tarjetaproducto-hija">
+        <Col key={key}>
+          <Card className="tarjetaproducto-hija" onClick={handleShow}>
             <Card.Img
               variant="top"
               src={image}
-              style={{ aspect_ratio: "1.0", height: 150, crop: "fit"}}              
+              style={{ aspect_ratio: "1.0", height: 150, crop: "fit"}}
             />
             <Card.Body>
               <Card.Title>{name}</Card.Title>
@@ -26,13 +31,33 @@ const ProductoItem = (props) => {
             </Card.Body>
           </Card>
         </Col>
-        </Row>
+      </Row>
 
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contactar a Capitán Jack</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Puedes hacerlo a través de:</p>
+          <p> Whatsapp
+          <a href='https://api.whatsapp.com/send?phone=3492317823' target="_blank" rel="noreferrer">
+               <i class='whatIcon'><ImWhatsapp/></i>
+          </a>
+          </p>
+          <p>
+            O llámanos al: <a class='whatIcon' href="tel:3492317823">3492317823</a>
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </button>
+        </Modal.Footer>
+      </Modal>
     </section>
-
-    );
+  );
 }
 
-
-
 export default ProductoItem;
+
+
